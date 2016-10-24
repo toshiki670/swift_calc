93acc6480739c20c954db740853cf42d6638fa17
Toshiki-670 <tsk_670@icloud.com>
2016-10-24 23:05:50 +0900

    Initial Commit

---
+++ Celc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Celc
+//
+//  Created by Toshiki on 2016/10/24.
+//  Copyright © 2016 Toshiki.Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
